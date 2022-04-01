15829dffd9183279300d8a4a74854349fe0d5e11
Edwin Villanueva <aquarius.tlv@gmail.com>
2022-04-01 00:52:32 -0300

    primeros avances en el back , falta mucho todavia

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,10 +3,41 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define('videogame', 
+  {
+    id:{
+      type:DataTypes.UUID,
+      allowNull:false,
+      primaryKey:true,
+      defaultValue:DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull:false,
       allowNull: false,
     },
+    description:{
+      type:DataTypes.TEXT,// ver si se debe cambiar el tipo de dato
+      allowNull:false,
+
+    },
+    releaseDate:{
+      type:DataTypes.DATEONLY,
+      defaultValue: DataTypes.NOW
+    },
+    rating:{
+      type:DataTypes.INTEGER,// ver si se debe cambiar el tipo de dato
+      allowNull:false,
+    },
+    platforms:{
+      type:DataTypes.TEXT,// ver si se debe cambiar el tipo de dato
+      allowNull:false,
+    }
+    
+
+  },
+  {//SE PUEDE SACAR O VOLVER A PONER ESTOS ATRIBUTOS QUE VIENEN POR DEFECTO
+    createdAt: false,
+    updatedAt: false,
   });
 };
